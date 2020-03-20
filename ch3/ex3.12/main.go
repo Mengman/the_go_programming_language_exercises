91dba5a56dd29651e175e6980023cb93bc856f7e
Mengman <corrsboy@gmail.com>
2020-03-20 11:40:16 +0800

    fix bug

--- ch3/ex3.12/main.go
+++ ch3/ex3.12/main.go
@@ -1,5 +1,8 @@
 package main
 
+// Exercis e 3.12: Write a function that reports whether two str ings are anagrams of each other,
+// that is, the y contain the same letters in a different order.
+
 import (
 	"flag"
 	"fmt"
@@ -10,16 +13,17 @@ func isAnagrams(s1, s2 string) bool {
 		return false
 	}
 
-	m := make(map[rune]bool)
-	for _, l := range s1 {
-		m[l] = true
-	}
+	rune1 := []rune(s1)
+	rune2 := []rune(s2)
 
-	for _, l := range s2 {
-		if _, ok := m[l]; !ok {
+	for i, r1 := range rune1 {
+		r2 := rune2[len(rune2)-1-i]
+
+		if r1 != r2 {
 			return false
 		}
 	}
+
 	return true
 }
 
