dcf3fd315395ab8a87bfd2fdd36d24492f8b61b4
Mengman <corrsboy@gmail.com>
2020-03-24 11:30:40 +0800

    fix ex4.1 bug

--- ch4/ex4.1/main.go
+++ ch4/ex4.1/main.go
@@ -1,21 +1,41 @@
 package main
 
+// Exercise 4.1: Write a function that counts the number of bits that are different in two SHA256
+// hashes. (See PopCount from Section 2.6.2.)
+
 import (
 	"crypto/sha256"
 	"fmt"
 	"os"
 )
 
+func popCount(b byte) int {
+	count := 0
+	for ; b != 0; count++ {
+		b &= b - 1
+	}
+
+	return count
+}
+
+func bitDiff(a, b []byte) int {
+	count := 0
+	for i := 0; i < len(a) || i < len(b); i++ {
+		if i >= len(a) || i >= len(b) {
+			count += 8
+		} else {
+			count += popCount(a[i] ^ b[i])
+		}
+	}
+
+	return count
+}
+
 func countSHA256Diff(a, b string) int {
 	c1 := sha256.Sum256([]byte(a))
 	c2 := sha256.Sum256([]byte(b))
-	diffNum := 0
-	for i := 0; i < len(c1); i++ {
-		if c1[i] != c2[i] {
-			diffNum++
-		}
-	}
-	return diffNum
+
+	return bitDiff(c1[:], c2[:])
 }
 
 func main() {
